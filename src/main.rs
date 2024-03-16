3e513f86ac45096aa55952f972a3da516cf66c27
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-03-16 16:29:54 +0900

    feat: fetch db records

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,22 @@ use axum::{
     routing::get,
     Router,
 };
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::env;
 use tokio::net::TcpListener;
 
 struct Query;
 
-#[async_graphql::Object]
+#[Object]
 impl Query {
-    async fn total_photos(&self) -> usize {
-        42
+    async fn total_photos<'ctx>(&self, ctx: &Context<'ctx>) -> i64 {
+        let connection = &mut ctx.data_unchecked::<Pool<Postgres>>();
+        let counts = sqlx::query!("SELECT COUNT(*) as count FROM photos")
+            .fetch_one(&**connection)
+            .await
+            .unwrap();
+        counts.count.unwrap()
     }
 }
 
@@ -25,7 +33,16 @@ async fn graphql_playground() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .unwrap();
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data(pool.clone())
+        .finish();
 
     // "/" でリクエストを待つ
     let app = Router::new().route(
