7eddd0a1fb31feb9f83e1321613df40bb0efe664
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-03-17 09:27:10 +0900

    feat: use data_unchecked

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ impl Query {
     async fn total_photos<'ctx>(&self, ctx: &Context<'ctx>) -> i64 {
         let connection = &mut ctx.data_unchecked::<Pool<Postgres>>();
         let counts = sqlx::query!("SELECT COUNT(*) as count FROM photos")
-            .fetch_one(&**connection)
+            .fetch_one(*connection)
             .await
             .unwrap();
         counts.count.unwrap()
